b45ddeb7ada1299d926e68ecedd11754b21f5679
Nguyen Tu <nguyentu98lt@gmail.com>
2025-07-09 10:13:47 +0700

    Initial commit

---
+++ app/src/main/java/com/example/eatwell/ui/home/HomeFragment.kt
@@ -0,0 +1,15 @@
+package com.example.eatwell.ui.home
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.eatwell.databinding.FragmentHomeBinding
+
+class HomeFragment : Fragment() {
+
+
+}
---
+++ app/src/main/java/com/example/eatwell/ui/slideshow/SlideshowFragment.kt
@@ -0,0 +1,34 @@
+package com.example.eatwell.ui.slideshow
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.webkit.WebView
+import androidx.fragment.app.Fragment
+import android.webkit.WebViewClient
+import com.example.eatwell.R
+
+class SlideshowFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val root = inflater.inflate(R.layout.fragment_slideshow, container, false)
+
+        val webView: WebView = root.findViewById(R.id.webview)
+
+        // Kích hoạt JavaScript nếu cần
+        webView.settings.javaScriptEnabled = true
+
+        // Đảm bảo mở trang web trong WebView thay vì trình duyệt ngoài
+        webView.webViewClient = WebViewClient()
+
+        // Load trang web
+        webView.loadUrl("http://duocphutho.edu.vn/category/khoa-hoc-cong-nghe/")
+
+        return root
+    }
+}
